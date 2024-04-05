ff4af630fe49ba279f9eeebf1568bb274b7f455b
Tanmoy Das <144507062+TanmoyDas04@users.noreply.github.com>
2024-04-05 21:10:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let no = document.getElementById("noBtn");
+let yes = document.getElementById("yesBtn");
+
+window.onload = function () {
+
+    yes.addEventListener("click", () => {
+        let msg = document.querySelector(".msg");
+        let img = document.querySelector(".img");
+        let btnBox = document.querySelector(".btnbox");
+        msg.innerHTML = "Hehehe, I knew it! 😁😘💖";
+        img.src = "happy.gif";
+        btnBox.style.visibility = "hidden";
+    });
+
+
+    function moveNoBtn() {
+        const randomX = Math.floor(Math.random() * (window.innerWidth - 200));
+        const randomY = Math.floor(Math.random() * (window.innerHeight - 200));
+
+        no.style.position = "absolute";
+        no.style.left = randomX + "px";
+        no.style.top = randomY + "px";
+    }
+
+    no.addEventListener("mouseenter", () => {
+        moveNoBtn();
+    });
+
+    no.addEventListener("click", () => {
+        moveNoBtn();
+    });
+}
